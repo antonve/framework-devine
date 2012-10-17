ba3838e39280289a1bdd13e9a26ed9e7f0afabf6
Anton Van Eechaute <mail@antonve.me>
2012-10-17 15:38:12 +0200

    added missing injectable class

---
+++ src/Devine/Framework/Injectable.php
@@ -0,0 +1,38 @@
+<?php
+
+// Injectable.php - make php services easily available in controllers
+// By Anton Van Eechaute
+
+namespace Devine\Framework;
+
+class Injectable
+{
+    private $services = array();
+
+    public function sget($name)
+    {
+        return array_key_exists($name, $this->services) ? $this->services[$name] : null;
+    }
+
+    public function register($services)
+    {
+        foreach ($services as $service) {
+            if (!$this->isValid($service)) {
+                throw new \Exception('Invalid service');
+            }
+
+            $this->services[$service['name']] = new $service['class']($service['config']);
+        }
+    }
+
+    public function isValid($service)
+    {
+        if (array_key_exists('name', $service)
+            && array_key_exists('class', $service)
+            && array_key_exists('config', $service)) {
+            return true;
+        }
+
+        return false;
+    }
+}

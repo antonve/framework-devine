931668e3752f5985996c5425f6a53454e8852b37
Anton Van Eechaute <mail@antonve.me>
2012-10-26 13:13:46 +0200

    Class to load bundles

--- src/Devine/Framework/BundleLoader.php
+++ src/Devine/Framework/BundleLoader.php
@@ -1,32 +1,108 @@
 <?php
 
-// ModuleLoader.php - make php services easily available in controllers
+// BundleLoader.php - make php services easily available in controllers
 // By Anton Van Eechaute
 
 namespace Devine\Framework;
 
-class ModuleLoader
+/**
+ * A class to load bundles
+ */
+class BundleLoader
 {
-    private $modules = array();
+    /**
+     * An array with strings that represent bundles
+     * @var array
+     */
+    private $bundles = array();
+
+    /**
+     * An array with loaded bundles
+     * @var array
+     */
     private $loaded = array();
 
-    function __construct($modules)
+    /**
+     * An array containing all the routes
+     * @var array
+     */
+    private $routes;
+
+    /**
+     *
+     * @var Collection
+     */
+    private $services;
+
+    /**
+     * @var BundleClassLoader
+     */
+    private $bundleClassLoader;
+
+    /**
+     * @param array $bundles
+     * @param BundleClassLoader $bundleClassLoader
+     */
+    function __construct($bundles, BundleClassLoader $bundleClassLoader)
     {
-        $this->modules = $modules;
+        $this->bundles = $bundles;
+        $this->routes = new RouteCollection();
+        $this->bundleClassLoader = $bundleClassLoader;
+        $this->services = new Collection();
     }
 
+    /**
+     * Load all configured bundles
+     * @param $path path to where bundles live
+     * @throws \Exception
+     */
     public function load($path)
     {
-        foreach ($this->modules as $module) {
-            trace($module);
-            if (file_exists($path . $module . '/config/config.php')) {
-                $this->loaded[] = (new Module(
-                    include ($path . $module . '/config/config.php')
-                ))->load();
+        foreach ($this->bundles as $bundle) {
+            if (file_exists($path . $bundle . '/config/config.php')) {
+                $bundle = new Bundle(
+                    array_merge(
+                        include ($path . $bundle . '/config/config.php'),
+                        array ('path' => $path . $bundle)
+                    )
+                );
+                $bundle->load();
+                $this->bundleClassLoader->register($bundle->getNamespace());
+
+                $this->loaded[] = $bundle;
             } else {
-                throw new \Exception('Module \'' . $path . $module . '\'not found.');
+                throw new \Exception('Bundle \'' . $path . $bundle . '\'not found.');
             }
         }
+    }
+
+    /**
+     * Get all the routes, if empty get all
+     * @return array
+     */
+    public function getRoutes()
+    {
+        if ($this->routes->isEmpty()) {
+            foreach ($this->loaded as $bundle) {
+                $this->routes->addCollection($bundle->getRoutes());
+            }
+        }
+
+        return $this->routes;
+    }
+
+    /**
+     * Get all services
+     */
+    public function getServices()
+    {
+        if ($this->services->isEmpty()) {
+            foreach ($this->loaded as $bundle) {
+                $this->services->addArray($bundle->getServices());
+            }
+        }
+
+        return $this->services;
     }
 
 }

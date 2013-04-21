29f7d6a0521c9f3050ac8d8cdc3ada6ba5af145b
Anton Van Eechaute <antonvaneechaute@Antons-MacBook-Pro.local>
2013-04-21 22:45:42 +0200

    added codekit

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "web/css"
+sass_dir = "assets/sass"
+images_dir = "web/images"
+javascripts_dir = "assets/js"
+fonts_dir = "web/fonts"
+
+output_style = :compressed
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass assets/sass scss && rm -rf sass && mv scss sass
